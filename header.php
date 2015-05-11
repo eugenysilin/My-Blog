57dc90896edb1e364bb5fea63dc51070767b6183
Silin Eugeny <silinea1981@gmail.com>
2015-05-11 21:45:26 +0300

    add favicon

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Blog</title>
+    <link rel="icon" type="image/png" href="favicon.jpg">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="style.min.css?v=1">
     <script async="async" type="text/javascript" src="script.min.js?v=1"></script>
@@ -29,30 +30,25 @@
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
                         <li<?=
-                        'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] == 'http://blog.my/index.php'
-                            ? ' class="active"' : '' ?>>
+                        'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] == 'http://blog.my/index.php' ? ' class="active"' : '' ?>>
                             <a href="index.php">
                                 Home
                             </a>
                         </li>
                         <li<?=
-                        'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] == 'http://blog.my/about.php'
-                            ? ' class="active"' : '' ?>>
+                        'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] == 'http://blog.my/about.php' ? ' class="active"' : '' ?>>
                             <a href="about.php">About Us</a>
                         </li>
                         <li<?=
-                        'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] == 'http://blog.my/blog.php'
-                            ? ' class="active"' : '' ?>>
+                        'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] == 'http://blog.my/blog.php' ? ' class="active"' : '' ?>>
                             <a href="blog.php">Blog</a>
                         </li>
                         <li<?=
-                        'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] == 'http://blog.my/gallery.php'
-                            ? ' class="active"' : '' ?>>
+                        'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] == 'http://blog.my/gallery.php' ? ' class="active"' : '' ?>>
                             <a href="gallery.php">Gallery</a>
                         </li>
                         <li<?=
-                        'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] == 'http://blog.my/contact.php'
-                            ? ' class="active"' : '' ?>>
+                        'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] == 'http://blog.my/contact.php' ? ' class="active"' : '' ?>>
                             <a href="contact.php">Contact Us</a>
                         </li>
                     </ul>
